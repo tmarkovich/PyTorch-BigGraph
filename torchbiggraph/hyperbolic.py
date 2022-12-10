dd83025785dc77c13f1ebb60eea53fa3ab7883d9
Thomas Markovich <tmarkovich@twitter.com>
2022-12-09 21:03:33 -0500

    Simplified fns

--- torchbiggraph/hyperbolic.py
+++ torchbiggraph/hyperbolic.py
@@ -2,13 +2,22 @@ import torch
 
 EPS = 1e-15
 BALL_EPS = {torch.float32: 4e-3, torch.float64: 1e-5}
+MAX = {torch.float32: 3.1, torch.float64: 6.1}
 
 def expmap0(x, c):
     arg = torch.sqrt(c) * x.norm(dim=-1, p=2, keepdim=True).clamp_min(EPS)
     pre = torch.tanh(arg) / (arg)
     return project(pre * x, c)
 
-def proj(x, c):
+def expmap0_guarded(x, c):
+    arg = torch.sqrt(c) * x.norm(dim=-1, p=2, keepdim=True).clamp_min(EPS)
+    pre = torch.where(
+            arg < MAX[x.dtype],
+            torch.atanh(arg),
+            1. - BALL_EPS[x.dtype]) / arg
+    return pre * x
+
+def project(x, c):
     norm = x.norm(dim=-1, p=2, keepdim=True).clamp_min(MIN_NORM)
     eps = BALL_EPS[x.dtype]
     maxnorm = (1 - eps) / (c ** 0.5)
@@ -16,23 +25,20 @@ def proj(x, c):
     projected = x / norm * maxnorm
     return torch.where(cond, projected, x)
 
-def mobius_add(x, c):
+def mobius_add(x, y, c):
     x2 = torch.sum(x * x, dim=-1, keepdim=True)
     y2 = torch.sum(y * y, dim=-1, keepdim=True)
     xy = torch.sum(x * y, dim=-1, keepdim=True)
-    num = (1 + 2 * c * xy + c * y2) * x + (1  -c * x2) * y
-    denom = (1 + 2 * c * xy + c ** 2 * x2 * y2).clamp_min(1e-15)
+    num = (1 + 2 * c * xy + c * y2) * x + (1  - c * x2) * y
+    denom = (1 + 2 * c * xy + c ** 2 * x2 * y2).clamp_min(EPS)
     return num / denom
 
 def hyp_distance(x, y, c, eval_mode=False):
-    """Hyperbolic distance on the Poincare ball with curvature c.
-    Args:
-        x: torch.Tensor of size B x d with hyperbolic queries
-        y: torch.Tensor with hyperbolic queries, shape n_entities x d if eval_mode is true else (B x d)
-        c: torch.Tensor of size 1 with absolute hyperbolic curvature
-    Returns: torch,Tensor with hyperbolic distances, size B x 1 if eval_mode is False
-            else B x n_entities matrix with all pairs distances
-    """
+    rootc = torch.sqrt(c)
+    arg = mobius_add(x, y, c).norm(dim=-1, p=2, keepdim=True)
+    return 2. * torch.atanh(-rootc * arg) / rootc
+
+def old_hyp_distance(x, y, c, eval_mode=False):
     sqrt_c = c ** 0.5
     x2 = torch.sum(x * x, dim=-1, keepdim=True)
     if eval_mode:
@@ -51,14 +57,6 @@ def hyp_distance(x, y, c, eval_mode=False):
 
 
 def hyp_distance_multi_c(x, v, c, eval_mode=False):
-    """Hyperbolic distance on Poincare balls with varying curvatures c.
-    Args:
-        x: torch.Tensor of size B x d with hyperbolic queries
-        y: torch.Tensor with hyperbolic queries, shape n_entities x d if eval_mode is true else (B x d)
-        c: torch.Tensor of size B x d with absolute hyperbolic curvatures
-    Return: torch,Tensor with hyperbolic distances, size B x 1 if eval_mode is False
-            else B x n_entities matrix with all pairs distances
-    """
     sqrt_c = c ** 0.5
     if eval_mode:
         vnorm = torch.norm(v, p=2, dim=-1, keepdim=True).transpose(0, 1)
@@ -75,7 +73,6 @@ def hyp_distance_multi_c(x, v, c, eval_mode=False):
     pairwise_norm = num / denom.clamp_min(MIN_NORM)
     dist = artanh(sqrt_c * pairwise_norm)
     return 2 * dist / sqrt_cdef rotation(r, x):
-    pass
 
 def rotation(r, x):
     givens = r.view((r.shape[0], -1, 2))
